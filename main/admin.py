de7c6643c80109764dc05931d45a1f67669f0a53
nadincahyani <nadin.ptr00@gmail.com>
2026-09-14 07:51:45 +0700

    fix: Tugas 2 (Menerapkan MVT Django)

--- main/admin.py
+++ main/admin.py
@@ -2,8 +2,5 @@ from django.contrib import admin
 from .models import Experience
 
 # Register your models here.
-@admin.register(Experience)
-class ExperienceAdmin(admin.ModelAdmin):
-    list_display = ('role', 'organization', 'category', 'is_ongoing', 'started_at')
-    list_filter = ('category', 'ended_at')
-    search_fields = ('role', 'organization', 'description')
+from main.models import Experience
+admin.site.register(Experience)
